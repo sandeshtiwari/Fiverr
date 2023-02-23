95552504dbdf76281ee8657f6dccbd8e12d49183
Sandesh Tiwari <sandeshtiwari@live.com>
2023-02-23 10:39:15 -0600

    feat: Add secondary navbar below

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import './Navbar.scss';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
     <div className="navbar">
       <div className="container">
         <div className="logo">
+          {/* <Link to="/"> */}
           <span className="text">fiverr</span>
+          {/* </Link> */}
           <span className="dot">.</span>
         </div>
         <div className="links">
@@ -17,6 +20,11 @@ const Navbar = () => {
           <span>Become a Seller</span>
           <button>Join</button>
         </div>
+      </div>
+      <hr />
+      <div className="menu">
+        <span>Test</span>
+        <span>Test2</span>
       </div>
     </div>
   );
