c8fd68fe6e99c6e763aac4cc203e521ae4e9a62f
Sandesh Tiwari <sandeshtiwari@live.com>
2023-03-07 15:40:21 -0600

    feat: Add infinite carousel for product slider

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -91,7 +91,7 @@ const Home = () => {
           </div>
         </div>
       </div>
-      <Slide>
+      <Slide slidesToShow={4} arrowsScroll={4}>
         {projects.map((card) => (
           <ProjectCard item={card} key={card.id} />
         ))}
