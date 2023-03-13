b4d66ca4a26d9c44fc69d888a1a10151f471cb9c
Sandesh Tiwari <sandeshtiwari@live.com>
2023-03-13 15:46:26 -0500

    feat: nodejs secure auth with JWT and cookies

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,1 +1,13 @@
-export const fn = (req, res) => {};
+import User from '../models/user.model.js';
+import jwt from 'jsonwebtoken';
+
+export const deleteUser = async (req, res) => {
+  const user = await User.findById(req.params.id);
+
+  if (req.userId !== user._id.toString()) {
+    return res.status(403).send('You can only delete your account!');
+  }
+  await User.findByIdAndDelete(req.params.id);
+  res.status(200).send('Deleted!');
+  //
+};
