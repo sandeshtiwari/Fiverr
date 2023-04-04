89f7bcf9f81fd2f9125fb0bf59b5a97a904e973c
Sandesh Tiwari <sandeshtiwari@live.com>
2023-04-04 16:20:38 -0500

    feat: Add dymanic reviews to gigs

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -14,6 +14,6 @@ export const deleteUser = async (req, res, next) => {
 
 export const getUser = async (req, res, next) => {
   const user = await User.findById(req.params.id);
-  console.log(user);
+  // console.log(user);
   res.status(200).send(user);
 };
--- api/routes/review.route.js
+++ api/routes/review.route.js
@@ -1,8 +1,16 @@
 import express from 'express';
+import {
+  createReview,
+  deleteReview,
+  getReviews,
+} from '../controllers/review.controller.js';
 import {} from '../controllers/user.controller.js';
+import { verifyToken } from '../middleware/jwt.js';
 
 const router = express.Router();
 
-router.get('/test');
+router.post('/', verifyToken, createReview);
+router.get('/:gigId', getReviews);
+router.delete('/:gigId', deleteReview);
 
 export default router;
