5d76629e76bde3e01aeff7ec6e05e7b973350f51
stiwari64 <stiwari64@gatech.edu>
2023-04-09 18:14:08 -0500

    feat: View all messages list with functionality

--- api/routes/conversation.route.js
+++ api/routes/conversation.route.js
@@ -1,8 +1,18 @@
 import express from 'express';
 import {} from '../controllers/user.controller.js';
+import { verifyToken } from "../middleware/jwt.js";
+import {
+  createConversations,
+  getConversations,
+  getSingleConversation,
+  updateConversation,
+} from "../controllers/conversation.controller.js";
 
 const router = express.Router();
 
-router.get('/test');
+router.get("/", verifyToken, getConversations);
+router.post("/", verifyToken, createConversations);
+router.get("/single/:id", verifyToken, getSingleConversation);
+router.put("/:id", verifyToken, updateConversation);
 
 export default router;
