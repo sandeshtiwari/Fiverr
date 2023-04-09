373abd0d8128c53f3b1ad614553dfb4cc464b460
stiwari64 <stiwari64@gatech.edu>
2023-04-08 20:17:38 -0500

    feat: CORS setup for all incoming connection

--- api/server.js
+++ api/server.js
@@ -25,7 +25,10 @@ const connect = async () => {
   }
 };
 
-app.use(cors({ origin: 'http://127.0.0.1:5173', credentials: true }));
+// app.use(cors({ origin: 'http://127.0.0.1:5173', credentials: true }));
+// app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "*", credentials: true }));
+
 app.use(express.json());
 app.use(cookieParser());
 
